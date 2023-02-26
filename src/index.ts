95490b6efb12b735363e3806649ead04e1f27da4
mina_72 <mina.mahdizadeh91@gmail.com>
2023-02-26 12:07:57 +0330

    create session and "/" route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, {Request, Response } from 'express'
 import { router } from './routes/loginRoute'
 import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
 
 const app = express()
 
+//middlewares
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieSession({ keys: ['abc'] }))
 app.use(router)
 
 app.listen (3000, () => {
--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -1,4 +1,8 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
+
+interface RequestWithBody extends Request {
+    body: { [key: string]: string | undefined }
+}
 
 const router = Router()
 
@@ -20,9 +24,38 @@ router.get('/login', (req, res) => {
     )
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
     const { email, password } = req.body
-    res.send ( email + ' ' + password)
+
+    if ( email && password && email === 'mina.com' && password === '123' ) {
+        // mark user as logged in
+        req.session = { loggedIn: true }
+
+        // redirect to '/' route
+        res.redirect ('/')
+    }
+    else {
+        res.send ('provide email')
+    }
+})
+
+router.get('/', (req: Request, res: Response) => {
+    // check if user is logged in or not: req.session is: if user has session, req.session.loggedIn is: if user is logged in
+    if (req.session && req.session.loggedIn){
+        res.send (`
+            <div>
+                <div> You are logged in </div>
+                <a href="/logout"> Logout </a>
+            </div>
+         `)
+    } else {
+        res.send (`
+            <div>
+                <div> You are not logged in </div>
+                <a href="/login"> Login </a>
+            </div>
+         `)
+    }
 })
 
 export { router }
